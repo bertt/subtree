159bcc9ffdf44d04a37b6d9d95d158b5c37d1c2b
Bert Temme <btemme@hotmail.com>
2023-01-18 18:07:02 +0100

    updating dependencies

--- samples/quadtreewriter/B3dmCreator.cs
+++ samples/quadtreewriter/B3dmCreator.cs
@@ -1,11 +1,13 @@
-﻿namespace quadtreewriter;
+﻿using B3dmCore;
+
+namespace quadtreewriter;
 
 public static class B3dmCreator
 {
-    public static B3dm.Tile.B3dm GetB3dm(List<Triangle> triangleCollection)
+    public static B3dm GetB3dm(List<Triangle> triangleCollection)
     {
         var bytes = GlbCreator.GetGlb(triangleCollection);
-        var b3dm = new B3dm.Tile.B3dm(bytes);
+        var b3dm = new B3dm(bytes);
 
         return b3dm;
     }
