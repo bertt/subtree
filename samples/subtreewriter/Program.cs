112aeb2f01c9d43a6938e263a6609254376e85a0
Bert Temme <btemme@hotmail.com>
2023-03-20 15:16:45 +0100

    semantics experiment

--- samples/subtreewriter/Program.cs
+++ samples/subtreewriter/Program.cs
@@ -1,4 +1,5 @@
 ﻿using subtree;
+using System.Text;
 
 WriteSample();
 
@@ -6,4 +7,14 @@ void WriteSample()
 {
     var bytes = SubtreeWriter.ToBytes("1", "1");
     File.WriteAllBytes("subtrees/0.0.0.subtree", bytes);
+
+    var fs = File.Create(@"subtrees/metadata.bin");
+
+    var height = (double)1;
+    var bytes1 = BitConverter.GetBytes(height);
+    var paddedBytes = BufferPadding.AddPadding(bytes1);
+
+    var writer = new BinaryWriter(fs);
+    writer.Write(paddedBytes);
+    fs.Close();
 }
