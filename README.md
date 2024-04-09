c9dd0788f5e015339770e39160fc12d48d2c647c
Bert Temme <btemme@hotmail.com>
2024-04-09 12:47:45 +0200

    no rounding of bounding volume

--- README.md
+++ README.md
@@ -23,6 +23,8 @@ File.WriteAllBytes("subtrees/0.0.0.subtree", bytes);
 
 ## History
 
+2023-04-09: version 1.4.7: no rounding of bounding volume box
+
 2023-04-04: Version 1.4.5, 1.4.6: Add ZMin and ZMax as Tile properties
 
 2023-02-21: Version 1.4.3, 1.4.4: Change error handling
--- src/subtree/BoundingVolume.cs
+++ src/subtree/BoundingVolume.cs
@@ -1,17 +1,5 @@
-﻿
-namespace subtree;
+﻿namespace subtree;
 public class Boundingvolume
 {
-    private double[] _region;
-    public double[] region
-    {
-        get
-        {
-            return _region;
-        }
-        set
-        {
-            _region = value.Select(d => Math.Round(d, 5)).ToArray();
-        }
-    }
+    public double[] region { get; set; }
 }
