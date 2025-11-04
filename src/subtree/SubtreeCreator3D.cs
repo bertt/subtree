bdbde90af0f8d1fd5a346cda5e2a2f59a1e8cc67
Bert Temme <btemme@hotmail.com>
2025-11-04 14:08:45 +0100

    add a stub for octree subtreewriter

---
+++ src/subtree/SubtreeCreator3D.cs
@@ -0,0 +1,11 @@
+﻿namespace subtree;
+
+public static class SubtreeCreator3D
+{
+    public static Dictionary<Tile, byte[]> GenerateSubtreefiles(List<Tile3D> tiles)
+    {
+        // todo: implement octree subtree generation
+        // For now, return an empty dictionary
+        return new Dictionary<Tile, byte[]>();
+    }
+}
