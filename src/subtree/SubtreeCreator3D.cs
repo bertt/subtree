e4eeab9ff333b2f79953dd7ef2d44b90670b9177
Bert Temme <btemme@hotmail.com>
2025-11-04 14:19:22 +0100

    fix signature

--- src/subtree/SubtreeCreator3D.cs
+++ src/subtree/SubtreeCreator3D.cs
@@ -2,10 +2,10 @@
 
 public static class SubtreeCreator3D
 {
-    public static Dictionary<Tile, byte[]> GenerateSubtreefiles(List<Tile3D> tiles)
+    public static Dictionary<Tile3D, byte[]> GenerateSubtreefiles(List<Tile3D> tiles)
     {
         // todo: implement octree subtree generation
         // For now, return an empty dictionary
-        return new Dictionary<Tile, byte[]>();
+        return new Dictionary<Tile3D, byte[]>();
     }
 }
